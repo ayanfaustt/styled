dadff3fb200be784715619b4689d9c8e60d410c7
ayanfaustt <ayanfaustt@gmail.com>
2022-06-07 10:42:02 -0300

    firts commit

---
+++ src/components/Circulo/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Container from "./style";
+
+function Circulo(){
+    return (
+        <Container>
+            conteúdo
+        </Container>
+        
+    )
+}
+
+export default Circulo;
---
+++ src/components/Circulo/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+
+const Container = styled.div`
+    background: red;
+    border-radius: 50%;
+    height: 500px;
+    width: 500px;
+`;
+
+export default Container;
